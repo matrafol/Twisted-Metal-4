9b6c2891e108105f01e21f7c9c2bf840f09aac4e
matlaptop <matrafol1@gmail.com>
2019-06-07 20:53:55 +0800

    - Change highlight on mouseover and change the value of the dom elements based on the target element

--- twisted_metal_4.js
+++ twisted_metal_4.js
@@ -15,20 +15,6 @@
 	'special_weapon_stats' :,
 	'speed' : ,
 
-// Carousel by button
-	index = 0;
-
-	if previous button is clicked
-		index--
-		call function changehtmlelement(index)
-		if index == 0
-			disable/hide previous button or goes to the last array
-	else
-		index++
-		call changehtmlelement(index)
-		if index == index.length - 1(last index)
-			disable/hide next button or goes to the first array
-
 // Search function
 	auto complete inside the search function displaying the name and vehicle of the character
 	if the user input does not match then alert an error or change the html element with search result not found
@@ -83,6 +69,21 @@
 			speed: 4, 
 			ending_video: '<iframe src="https://www.youtube.com/embed/7ACQyrZJ4zM" \
 				frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+		},
+		{
+			index: 3,
+			name: "Crusher",
+			type_of_car: "Test",
+			vehicle_image: '<img src="images/characters/bosses/crusher-vehicle.png">',
+			driver_deamanor: "Twisted",
+			special_weapon_desc: 
+				"Drag Queen's flame thrower projects forward from the car's grille and sets opponents on fire",
+			handling: 1,
+			armor: 2,
+			special_weapon_stats: 3,
+			speed: 4, 
+			ending_video: '<iframe src="https://www.youtube.com/embed/7ACQyrZJ4zM" \
+				frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 		}
 	];
 	//return characters[index];
@@ -92,7 +93,6 @@ var index = 0;
 const characterLength = characters.length;
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
-const search = document.getElementById("search");
 
 nextButton.addEventListener("click", function(){
 	if(index == characterLength - 1){
@@ -116,6 +116,7 @@ previousButton.addEventListener("click", function(){
 	}
 });
 
+const search = document.getElementById("search");
 const autoCompleteResult = document.getElementById("auto_complete_result");
 var matches = [];
 var focusHighlight = 0;
@@ -131,32 +132,6 @@ search.addEventListener("keyup", function(e){
 			displayMatches(matches);
 		}
 	}
-	//console.log(matches);
-	//console.log(autoCompleteResult.children);
-	// Change the focusHighlight based on mouse hover
-
-	/*var autoCompleteChildren = autoCompleteResult;
-	autoCompleteChildren.addEventListener("mouseenter", function(e){
-		alert(e.value);
-		//document.getElementsByClassName("result_index")[focusHighlight].value
-		//highlightResult(selectedIndex);
-		if(focusHighlight > 0){
-			highlightResult(focusHighlight);
-			focusHighlight--;
-		} else if(focusHighlight < matches.length - 1){
-			highlightResult(focusHighlight);
-			focusHighlight++;
-		}
-	});*/
-
-	/*var test = document.getElementsByClassName("result")[0];
-	test.addEventListener("mouseenter", function(e){
-		e.classList.add("highlighted");
-	});
-
-	test.addEventListener("mouseout", function(e){
-		e.classList.remove("highlighted");
-	});*/
 
 	document.body.addEventListener('click', function(e){
 		toggleResult("hide");
@@ -166,7 +141,7 @@ search.addEventListener("keyup", function(e){
 		switch(e.key){
 			case "Enter":
 				// Changes the text inside search
-				search.value = document.getElementsByClassName("result")[focusHighlight].children[1].innerHTML;
+				search.value = document.getElementsByClassName("result_name")[focusHighlight].innerHTML;
 				index = document.getElementsByClassName("result_index")[focusHighlight].value;
 				changeHtmlElement(characters[index]);
 				toggleResult("hide");
@@ -174,18 +149,34 @@ search.addEventListener("keyup", function(e){
 				e.preventDefault();
 				break;
 			case "ArrowUp":
+				// As long as the focusHighlight isnt equal to 0, decrease index by 1 then highlight
 				if(focusHighlight > 0){
 					focusHighlight--;
 					highlightResult(focusHighlight);
 				}
 				break; 
 			case "ArrowDown":
+				// As long as the focusHighlight isnt equal to the last matches index, increase the index then highlight
 				if(focusHighlight < matches.length - 1){
 					focusHighlight++;
 					highlightResult(focusHighlight);
 				}
 				break;
 		}
+		// Add event listener to autocomplete result on hover
+		autoCompleteResult.addEventListener("mouseover", function(e){
+			// Get the index number of one of the result on hover
+			focusHighlight = e.target.getElementsByClassName("matches_index")[0].value;
+			highlightResult(focusHighlight);
+			
+			e.target.addEventListener("click", function(e){
+				search.value = this.getElementsByClassName("result_name")[0].innerHTML;
+				index = this.getElementsByClassName("result_index")[0].value;
+				changeHtmlElement(characters[index]);
+				toggleResult("hide");
+				focusHighlight = 0;
+			});
+		});
 	}
 });
 
@@ -202,24 +193,22 @@ function getMatches(input){
 	var matchList = [];
 
 	for (var i = 0; i < characterLength; i++) {
-		// indexOf method returns -1 if it didnt find any match
 		// Compares if there is a match between the input text and name object in the characters array
-		//if(characters[i].name.toLowerCase().indexOf(input.toLowerCase()) != -1){
 		if(characters[i].name.toLowerCase().startsWith(input)){
 			// populate the matchList empty array
 			matchList.push(characters[i]);
-			//index = i; BUGGED
 		}
 	}
 	return matchList;
 }
 
 // Populate the ul html element if theres a match
-function displayMatches(matches){
+function displayMatches(matches){	
 	for (var matchCounter = 0; matchCounter < matches.length; matchCounter++) {
 		// Create li element inside ul with the name from the characters array
 		autoCompleteResult.innerHTML += '<li class="result">' + matches[matchCounter].vehicle_image + '' +
 										'<span class="result_name">' + matches[matchCounter].name +'</span>' +
+										'<input type="hidden" class="matches_index" value="'+ matchCounter +'">' +
 										'<input type="hidden" class="result_index" value="'+ matches[matchCounter].index +'"></li>';
 	}
 	highlightResult(focusHighlight);
@@ -254,16 +243,6 @@ function changeHtmlElement(characterData){
 	}
 	return elements;
 }
-
-//changeHtmlElement(characters[1]);
-//console.log(changeHtmlElement(characters[1]));
-//console.log(changeHtmlElement(getCharacterData(1)));
-
-
-/*for (var data in getCharacterData(2)) {
-	console.log(getCharacterData(2)[data]);
-}*/
-
 
 // get the character data from function then return the object value that you want
 /*for (var speed_stat = 0; speed_stat < getCharacterData(0).speed; speed_stat++) {
