b0246cebfbb7c6bd149d2a63d12c593a94fee73c
matlaptop <matrafol1@gmail.com>
2019-06-08 18:18:59 +0800

    - Created a fuction to hide character info and ending secion if the characters player type is boss - - Added new functions inside the elements object array that creates new div element and append it to its respective parent node

--- twisted_metal_4.js
+++ twisted_metal_4.js
@@ -1,6 +1,7 @@
 const characters = [
 	{
 		index: 0,
+		player_type: "Contestant",
 		name: "Calypso",
 		type_of_car: "Nuke Mobile",
 		vehicle_image: '<img src="images/characters/calypso/calypso-vehicle.png">',
@@ -16,6 +17,7 @@ const characters = [
 	},
 	{
 		index: 1,
+		player_type: "Contestant",
 		name: "Captain Grimm",
 		type_of_car: "Pirate ship car",
 		vehicle_image: '<img src="images/characters/captain-grimm/captain-grimm-vehicle.png">',
@@ -31,6 +33,7 @@ const characters = [
 	}, 
 	{
 		index: 2,
+		player_type: "Contestant",
 		name: "Drag Queen",
 		type_of_car: "Hot Rodder",
 		vehicle_image: '<img src="images/characters/drag-queen/drag-queen-vehicle.png">',
@@ -43,6 +46,22 @@ const characters = [
 		speed: 4, 
 		ending_video: '<iframe src="https://www.youtube.com/embed/7ACQyrZJ4zM" \
 			frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		index: 3,
+		player_type: "Boss",
+		name: "Crusher",
+		//type_of_car: "Hot Rodder",
+		vehicle_image: '<img src="images/characters/bosses/crusher-vehicle.png">',
+		//driver_deamanor: "Twisted",
+		//special_weapon_desc: 
+			//"Drag Queen's flame thrower projects forward from the car's grille and sets opponents on fire",
+		//handling: 1,
+		//armor: 2,
+		//special_weapon_stats: 3,
+		//speed: 4, 
+		//ending_video: '<iframe src="https://www.youtube.com/embed/7ACQyrZJ4zM" \
+			//frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 	},
 ];
 
@@ -90,10 +109,6 @@ search.addEventListener("keyup", function(e){
 		}
 	}
 
-	document.body.addEventListener('click', function(e){
-		toggleResult("hide");
-	});
-
 	if(autoCompleteResult.classList.contains('visible')){
 		switch(e.key){
 			case "Enter":
@@ -133,6 +148,11 @@ search.addEventListener("keyup", function(e){
 			});
 		});
 	}
+});
+
+document.body.addEventListener('click', function(e){
+		toggleResult("hide");
+		focusHighlight = 0;
 });
 
 function toggleResult(action){
@@ -177,34 +197,74 @@ function highlightResult(highlightedIndex){
 	autoCompleteResult.children[highlightedIndex].classList.add("highlighted");
 }
 
+function toggleCharacterInfoEnding(playerType){
+	var characterInfo = document.getElementsByClassName("character_info")[0];
+	var characterEnding = document.getElementsByClassName("character_ending")[0];
+
+	if(playerType.toLowerCase() == "boss"){
+		characterInfo.style.display = "none";
+		characterEnding.style.display = "none";
+	} else{
+		characterInfo.style.display = "block";
+		characterEnding.style.display = "block";
+	}
+}
+
 function changeHtmlElement(characterData){
+	toggleCharacterInfoEnding(characterData.player_type);
+
+	var handlingHtml = document.getElementById("handling");
+	var armorHtml = document.getElementById("armor");
+	var specialWeaponHtml = document.getElementById("stats_special_weapon");
+	var speedHtml = document.getElementById("speed");
+
 	var elements ={
 		name: document.getElementsByClassName("character_name")[0].innerHTML = characterData.name,
 		type_of_car: document.getElementById("type_of_car").innerHTML = characterData.type_of_car,
 		driver_deamanor: document.getElementById("driver_demeanor").innerHTML = characterData.driver_deamanor,
 		special_weapon_desc: document.getElementById("special_weapon_desc").innerHTML = characterData.special_weapon_desc,
-		/*handling: function(characterData){
-			for (var handlingStat = 0; handlingStat < characterData.handling; handlingStat++) {
+
+		// Javascript closure, pass the needed characterData variable to the functions
+		// Keep createing new element div with a class name of stats 
+		// Then append it to its parent node until the counter is equal to the passed variable
+
+		handling: (function(handling){
+			handlingHtml.innerHTML = '';
+			for (var handlingStat = 0; handlingStat < handling; handlingStat++) {
 				var stat = document.createElement("div");
 				stat.className = "stats";
-				document.getElementById("handling").appendChild(stat);
+				handlingHtml.appendChild(stat);
 			}
-		},*/
-		armor: characterData.armor,
-		special_weapon_stats: characterData.special_weapon_stats,
-		speed: characterData.speed,
+		}(characterData.handling)),
+		armor: (function(armor){
+			armorHtml.innerHTML = '';
+			for (var armorStat = 0; armorStat < armor; armorStat++) {
+				var stat = document.createElement("div");
+				stat.className = "stats";
+				armorHtml.appendChild(stat);
+			}
+		}(characterData.armor)),
+		special_weapon_stats: (function(specialWeapon){
+			specialWeaponHtml.innerHTML = '';
+			for (var specialWeaponStat = 0; specialWeaponStat < specialWeapon; specialWeaponStat++) {
+				var stat = document.createElement("div");
+				stat.className = "stats";
+				specialWeaponHtml.appendChild(stat);
+			}
+		}(characterData.special_weapon_stats)),
+		speed: (function(speed){
+			speedHtml.innerHTML = '';
+			for (var speedStat = 0; speedStat < speed; speedStat++) {
+				var stat = document.createElement("div");
+				stat.className = "stats";
+				speedHtml.appendChild(stat);
+			}
+		}(characterData.speed)),
 		ending_video: document.getElementById("ending").innerHTML = characterData.ending_video
 	}
 	return elements;
 }
-
-// get the character data from function then return the object value that you want
-/*for (var speed_stat = 0; speed_stat < getCharacterData(0).speed; speed_stat++) {
-	var stat = document.createElement("div");
-	stat.className = "stats";
-	document.getElementById("speed").appendChild(stat);
-}*/
-//console.log(getCharacterData(1).speed);
 	
+window.onload = changeHtmlElement(characters[index]);
 
 
